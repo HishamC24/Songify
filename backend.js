a00e86f797e30060cc20b55a6151c7b98a24a40f
Zergling25 <mharrie@oakland.edu>
2025-11-04 19:35:46 -0500

    Refactor songs array and update exports
    Moved songs array to the top level and exported it.

--- backend.js
+++ backend.js
@@ -1,15 +1,17 @@
+const songs = [
+  "Can't Hold Us Macklemore",
+  "GTA 2 Rarin",
+  "Assumptions Sam Gellaitry",
+  "Levitating Dua Lipa",
+  "Loyal Odesza",
+  "How Long Charlie Puth",
+  "Hyperspace Sam I"
+];
+
 function requestSong() {
-  const songs = [
-    "Can't Hold Us Macklemore",
-    "GTA 2 Rarin",
-    "Assumptions Sam Gellaitry",
-    "Levitating Dua Lipa",
-    "Loyal Odesza",
-    "How Long Charlie Puth",
-    "Hyperspace Sam I"
-  ];
   return songs[Math.floor(Math.random() * songs.length)];
 }
 
-export { requestSong };
+export { songs, requestSong };
+
 
