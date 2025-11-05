9725860733a1e80d75a26eccec9a40553439e978
Zergling25 <mharrie@oakland.edu>
2025-11-04 19:22:32 -0500

    reformat

--- backend.js
+++ backend.js
@@ -1,28 +1,29 @@
 function requestSong() {
-    const songs = [
-        "Can't Hold Us Macklemore",
-        "GTA 2 Rarin",
-        "Assumptions Sam Gellaitry",
-        "Levitating Dua Lipa",
-        "Loyal Odesza",
-        "How Long Charlie Puth",
-        "Hyperspace Sam I"
-    ]
-    return songs[Math.floor(Math.random() * songs.length)];
+  const songs = [
+    "Can't Hold Us Macklemore",
+    "GTA 2 Rarin",
+    "Assumptions Sam Gellaitry",
+    "Levitating Dua Lipa",
+    "Loyal Odesza",
+    "How Long Charlie Puth",
+    "Hyperspace Sam I"
+  ];
+  return songs[Math.floor(Math.random() * songs.length)];
 }
 
+// ❤️ Like system (simple local storage)
 let likedSongs = JSON.parse(localStorage.getItem("likedSongs")) || [];
 
 function likeSong(songName) {
   const index = likedSongs.indexOf(songName);
   if (index !== -1) {
-    // Unlike
     likedSongs.splice(index, 1);
   } else {
-    // Like
     likedSongs.push(songName);
   }
   localStorage.setItem("likedSongs", JSON.stringify(likedSongs));
   return likedSongs;
 }
-export { requestSong, likesong };
+
+export { requestSong, likeSong };
+
